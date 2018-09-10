9d52afcd53bdbc1bb62797236b460d5e1664e90c
Joey Leger <legerinvestments@gmail.com>
2018-09-10 19:19:39 -0400

    [ADD] Added Header, Content and Footer components

---
+++ src/components/Content/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        I'm the content!!
+      </div>
+    );
+  }
+}
+
+export default Content;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        I'm the header!!
+      </div>
+    );
+  }
+}
+
+export default Header;
