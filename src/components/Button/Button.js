f14f8ff86a8c082fdb74278ccfaf1e54a172a865
Joey Leger <legerinvestments@gmail.com>
2018-09-20 20:46:20 -0400

    [ADD] Added menu toggle to header

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,12 @@ import './Button.css';
 
 class Button extends Component {
   render() {
-    return <button className={this.props.buttonClass}><span className="button__header__hamburger"></span><span className={this.props.buttonTextClass}>{this.props.buttonText}</span></button>;
+    return (
+      <button className={this.props.buttonClass} onClick={this.props.onClick}>
+        <span className="button__header__hamburger"></span>
+        <span className={this.props.buttonTextClass}>{this.props.buttonText}</span>
+      </button>
+    );
   }
 }
 
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -4,13 +4,28 @@ import Nav from '../../components/Nav/Nav';
 import Button from '../../components/Button/Button';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.state = {
+      active: false
+    }
+  }
+
+  toggleMenu() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+    // toggle nav menu
+  }
+
   render() {
     return (
       <div className="header">
         <div className="header__container">
           <div className="header__logo"></div>
-          <Button buttonText="Main Menu"
-                  buttonClass="button__header button__header--off"
+          <Button onClick={this.toggleMenu}
+                  buttonText="Main Menu"
+                  buttonClass={`button__header button__header--${this.state.active ? 'off': 'on'}`}
                   buttonTextClass="button__header__text" />
         </div>
         <Nav />
