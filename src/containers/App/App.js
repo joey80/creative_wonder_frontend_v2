d9dee4d1ee5ac04a7570d20e1086df722b2863cc
Joey <legerinvestments@gmail.com>
2019-03-10 21:02:41 -0400

    [REFACTOR] Added webfontloader and removed calls to google fonts

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,6 +8,7 @@ import Content from '../Content/Content';
 import Footer from '../Footer/Footer';
 
 class App extends Component {
+
   render() {
     return (
       <div className="app">
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { render } from 'react-snapshot';
 import './index.css';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: ['Homemade Apple', 'PT Sans Narrow']
+    }
+});
 
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
