b3a66e6bb41f980ab10bbcfd7098f6ee3aa1c1b6
Joey Leger <legerinvestments@gmail.com>
2019-03-24 13:51:33 -0400

    [FIX] Fixed YouTube embed issue

--- src/containers/Intro/Intro.js
+++ src/containers/Intro/Intro.js
@@ -11,14 +11,14 @@ class Intro extends Component {
             </Title>
             <div className="intro__container">
                 <div className="intro__video">
-                    <iframe src='http://www.youtube.com/embed/PZY-hB2C_Iw?wmode=transparent' allowFullScreen className="no-border" title="Preschool Video"></iframe>
+                <iframe src="https://www.youtube.com/embed/PZY-hB2C_Iw" frameborder="0" allow="accelerometer; gyroscope;" allowfullscreen className="no-border" title="Preschool Video"></iframe>
                 </div>
                 <div className="intro__content">
                     <p>Creative Wonder Early Learning Center provides a high standard of care, education and learning opportunities.
                     Children are able to explore a rich and <a href="index.html" >diverse range of activities</a> within a flexible, stimulating, inclusive
                     and welcoming learning environment. We aim to help the children develop self-respect, respect for others and
                     respect for their environment, to fufill their full potential and achieve the outcomes of Every Child Matters.</p>
-                    <p>Children develop in a fun and relaxed environment. The involvement of parents/carers in their child's
+                    <p>Children develop in a fun and relaxed environment. The involvement of parents in their child's
                     preschool experience plays a crucial role in building a firm foundation for future education.</p>
                 </div>
             </div>
