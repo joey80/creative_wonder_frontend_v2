03b4be3ab38507b6062e22dca6450d822e052fff
Joey <legerinvestments@gmail.com>
2020-03-12 19:11:53 -0400

    fix: refactored Nav component"

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
-import './Nav.css';
+import React from 'react';
+import './Nav.scss';
 
-class Nav extends Component {
-  render() {
-    return (
-      <div className={`nav nav--${this.props.isMenuOpen ? "down" : "hidden"}`}>
-        <div className="nav__container">
-          <ul className={`nav__list__first ${this.props.areLinksVisible ? "nav--down" : ""}`}>
-            <li>Home</li>
-            <li>About</li>
-            <li>Examples</li>
-            <li className="nav__arrow">Blog
-              <ul className="nav__list__second">
-                <li>Nintendo</li>
-                <li>Sony</li>
-                <li>Microsoft</li>
-              </ul>
-            </li>
-            <li className="nav__arrow">Gallery
-              <ul className="nav__list__second">
-                <li>Go To Bed</li>
-                <li>Brush Teeth</li>
-                <li>Floss</li>
-                <li>Jump Rope</li>
-              </ul>
-            </li>
-            <li>Contact Us</li>
+const Nav = ({ areLinksVisible, isMenuOpen }) => (
+  <div className={`nav nav--${isMenuOpen ? 'down' : 'hidden'}`}>
+    <div className='nav__container'>
+      <ul className={`nav__list__first ${areLinksVisible ? 'nav--down' : ''}`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Examples</li>
+        <li className='nav__arrow'>
+          Blog
+          <ul className='nav__list__second'>
+            <li>Nintendo</li>
+            <li>Sony</li>
+            <li>Microsoft</li>
           </ul>
-        </div>
-      </div>
-    );
-  }
-}
+        </li>
+        <li className='nav__arrow'>
+          Gallery
+          <ul className='nav__list__second'>
+            <li>Go To Bed</li>
+            <li>Brush Teeth</li>
+            <li>Floss</li>
+            <li>Jump Rope</li>
+          </ul>
+        </li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  </div>
+);
 
 export default Nav;
