bd4f808e98c62f6c54551b5db99d1507343a8cfe
Joey <legerinvestments@gmail.com>
2020-03-13 12:01:50 -0400

    fix: fixed blog data and component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => (
   <section className='blog'>
     <Title>Latest News</Title>
     <div className='blog__container'>
-      {Data.map({ date, description, imageLink, link, title }, idx => (
+      {Data.map(({ date, description, imageLink, link, title }, idx) => (
         <Card key={idx} blog imgUrl={imageLink} title={title} date={date} link={link}>
           {description}
         </Card>
