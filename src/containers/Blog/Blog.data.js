e8721ccaf8459c2136a3b725012ecc769cec3a0b
Joey <legerinvestments@gmail.com>
2020-03-13 10:43:39 -0400

    fix: refactored Card component

---
+++ src/containers/Blog/Blog.data.js
@@ -0,0 +1,18 @@
+export const Data = [
+         {
+           imageLink:
+             'https://res.cloudinary.com/hwzdnifrp/image/upload/c_scale,w_899/v1552164379/cw-blog1.jpg',
+           title: 'The Power Of Play In The Early Learning Environment',
+           date: 'Mar 1st 2019',
+           link: '#',
+           description: '"How will my child be prepared for school if all he does is play?" This is a fair and common question that is often asked and it's a question that can often be difficult for preschool teachers to answer. To sum up the value of play in the learning process in one short answer is not so easy to do'
+         },
+         {
+           imageLink:
+             'https://res.cloudinary.com/hwzdnifrp/image/upload/c_scale,w_681/v1552164293/cw-blog2.jpg',
+           title: 'Teaching Children To Ask For Help',
+           date: 'Mar 6th 2019',
+           link: '#',
+           description: 'In our preschools and at home, we should make "come to me for help" one of the first strategies that we teach children. Too often, I hear teachers and parents tell their children to go and figure it out without saying, "And I am here to help.'
+         }
+       ];
