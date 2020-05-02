56a36bdd2ac3e4e948cf2afda1edd4c1ef8a4907
Joey <legerinvestments@gmail.com>
2020-05-02 16:45:15 -0400

    fix: added toggle for faq item

--- src/components/FAQItem/FAQItem.js
+++ src/components/FAQItem/FAQItem.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import './FAQItem.scss';
 
-const FAQItem = ({ answer, color, question }) => (
-  <div className='FAQItem__container'>
-    <span className='FAQItem__question'>{question}</span>
-    <span className='FAQItem__answer'>
-      <p>{answer}</p>
-    </span>
-  </div>
-);
+const FAQItem = ({ answer, color, question }) => {
+  const faqRef = useRef(null);
+
+  const handleClick = () => {
+    faqRef.current.classList.toggle('faq-item--show');
+  };
+
+  return (
+    <div
+      className='faq-item__container'
+      ref={faqRef}
+      style={{ backgroundColor: `${color}` }}
+      onClick={handleClick}
+    >
+      <span className='faq-item__question'>{question}</span>
+      <span className='faq-item__answer'>
+        <p>{answer}</p>
+      </span>
+    </div>
+  );
+};
 
 export default FAQItem;
--- src/containers/FAQ/FAQ.data.js
+++ src/containers/FAQ/FAQ.data.js
@@ -3,31 +3,31 @@ export const Data = [
     answer:
       'Please do not send a sick child to school. If a child arrives with symptoms of illness, the child will not be permitted to stay. Children will be excluded from contact with the other children; seated with the staff at the front desk until parents can be notified. Please notify us if your child becomes ill and will not be attending that day. We ask that parents notify us when their child contracts a communicable disease. The preschool will notify all other parents about any communicable disease outbreaks (This does not name the child involved just the classroom to indicate highest exposure). A child will not be permitted to attend preschool when he or she have vomited or had diarrhea during the previous 24 hours. Medication may not be administered during program hours, therefore, do not send any in with your child.',
     color: '#e33b43',
-    question: 'What should I do if my child is sick?'
+    question: 'What should I do if my child is sick?',
   },
   {
     answer:
       'You will receive a calendar of events each month for you to review. Back-to-School Nights are held in October to give parents a chance to hear about the classroom routine and talk with the teachers without the children present. Periodically, teachers will send home Teacher Comments’ discussing your child’s adjustment to and enjoyment of the classroom routine. Parent-teacher conferences are available upon request.',
     color: '#2ef2f2',
-    question: "How can I keep up with what's going on with my child in class?"
+    question: "How can I keep up with what's going on with my child in class?",
   },
   {
     answer:
       "Yes we do. We have a music teacher and a Spanish teacher on our faculty. Both conduct morning classes Monday through Friday to provide continuity in the learning experience. Music and language experiences in the early years are very important.</p><p>We start toddlers in our music class to encourage the appreciation of singing and playing and listening to instruments. Music enhances brain development, especially spatial orientation and the ability to think mathematically. Our music teacher also includes many movement and literacy activities in the class. The brain is most receptive to learning languages during early childhood. That's why we start two-year-old children in Spanish class.",
     color: '#b6ec64',
-    question: 'Does Creative Wonder have specialty teachers?'
+    question: 'Does Creative Wonder have specialty teachers?',
   },
   {
     answer:
       'If someone else is picking up your child, you must inform Peartree in writing and in person. Send us a note detailing your plans, including the name of the person you are authorizing to pick-up your child and the date of its effectiveness. Please inform any escorts that we will check picture IDs to ensure they are authorized to pick-up your child. No one under the age of 18 may escort a child from Peartree. We cannot accept telephone authorizations. We will not send a child home with someone we do not know. Please introduce us to anyone who may be in to pick-up your child.',
     color: '#f2a341',
     question:
-      "I won't be available to pick-up my child today, so I'm arranging for my oldest son to pick him up. Do I need to send a note?"
+      "I won't be available to pick-up my child today, so I'm arranging for my oldest son to pick him up. Do I need to send a note?",
   },
   {
     answer:
       'Please do not send a sick child to school. If a child arrives with symptoms of illness, the child will not be permitted to stay. Children will be excluded from contact with the other children; seated with the staff at the front desk until parents can be notified. Please notify us if your child becomes ill and will not be attending that day. We ask that parents notify us when their child contracts a communicable disease. The preschool will notify all other parents about any communicable disease outbreaks (This does not name the child involved just the classroom to indicate highest exposure). A child will not be permitted to attend preschool when he or she have vomited or had diarrhea during the previous 24 hours. Medication may not be administered during program hours, therefore, do not send any in with your child.',
     color: '#c37cf5',
-    question: 'What should I do if my child is sick?'
-  }
+    question: 'What should I do if my child is sick?',
+  },
 ];
