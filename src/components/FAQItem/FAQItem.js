2558c4668834cb349608adb28e8d84ad01316f53
Joey <legerinvestments@gmail.com>
2020-05-02 21:26:17 -0400

    feat: added faq stylings and animations

--- src/components/FAQItem/FAQItem.js
+++ src/components/FAQItem/FAQItem.js
@@ -1,23 +1,18 @@
 import React, { useRef, useState } from 'react';
 import './FAQItem.scss';
 
-const FAQItem = ({ answer, color, question }) => {
+const FAQItem = ({ answer, num, question }) => {
   const faqRef = useRef(null);
 
   const handleClick = () => {
-    faqRef.current.classList.toggle('faq-item--show');
+    faqRef.current.children[1].classList.toggle('faq-item__answer--open');
   };
 
   return (
-    <div
-      className='faq-item__container'
-      ref={faqRef}
-      style={{ backgroundColor: `${color}` }}
-      onClick={handleClick}
-    >
-      <span className='faq-item__question'>{question}</span>
+    <div className='faq-item__container' ref={faqRef} onClick={handleClick}>
+      <span className={`faq-item__question faq-item__question--${num}`}>{question}</span>
       <span className='faq-item__answer'>
-        <p>{answer}</p>
+        <p class='faq-item__answer__text'>{answer}</p>
       </span>
     </div>
   );
