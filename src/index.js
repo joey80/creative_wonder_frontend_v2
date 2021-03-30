9ea743542311f0c85f4366da6c746496cbd78992
Joey Leger <joseph.leger@duke-energy.com>
2021-03-29 21:15:04 -0400

    fix: updated className prop in FAQItem

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import { render } from 'react-snapshot';
-import './index.css';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import WebFont from 'webfontloader';
+import './index.css';
 
 WebFont.load({
-    google: {
-        families: ['Homemade Apple', 'PT Sans Narrow']
-    }
+  google: {
+    families: ['Homemade Apple', 'PT Sans Narrow'],
+  },
 });
 
 render(<App />, document.getElementById('root'));
