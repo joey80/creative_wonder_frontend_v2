ebb5b27164c9d61c3814a8c8d3696b24c48b5e20
Joey Leger <joseph.leger@duke-energy.com>
2021-03-29 22:11:35 -0400

    feat: added Lato fontsource

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import '@fontsource/homemade-apple';
+import '@fontsource/lato';
 import '@fontsource/pt-sans-narrow';
 import './index.scss';
 
