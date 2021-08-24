a093780a93e462ef7d14c8698938394fa8246bfd
Joey Leger <joseph.leger@duke-energy.com>
2021-08-23 22:26:10 -0400

    added tests and stories to buttons

---
+++ src/components/Button/index.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Button, NavButton } from './index';
+
+describe('Button', () => {
+  it('renders the correct class variants', () => {
+    const { rerender } = render(<Button />);
+    expect(screen.getByTestId('button')).toHaveClass('button');
+    expect(screen.getByTestId('button')).not.toHaveClass('button--small');
+
+    rerender(<Button variant="small" />);
+    expect(screen.getByTestId('button')).toHaveClass('button--small');
+  });
+});
+
+describe('NavButton', () => {
+  it('renders the correct class variants', () => {
+    const { rerender } = render(<NavButton isMenuOpen={false} />);
+    expect(screen.getByTestId('navbutton')).toHaveClass('button__header button__header--off');
+    expect(screen.getByTestId('navbutton')).not.toHaveClass('button__header--on');
+
+    rerender(<NavButton isMenuOpen={true} />);
+    expect(screen.getByTestId('navbutton')).toHaveClass('button__header--on');
+  });
+});
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,11 @@ import { ButtonType, NavButtonType } from './types';
 import './styles.scss';
 
 const Button: ButtonType = ({ children, onClick, variant }) => (
-  <button className={`button ${variant ? `button--${variant}` : ''}`} {...{ onClick }}>
+  <button
+    className={`button ${variant ? `button--${variant}` : ''}`}
+    data-testid="button"
+    {...{ onClick }}
+  >
     {children}
   </button>
 );
@@ -12,6 +16,7 @@ const NavButton: NavButtonType = ({ children, isMenuOpen, onClick }) => (
   <div className="button__header__container">
     <button
       className={`button__header button__header--${isMenuOpen ? 'on' : 'off'}`}
+      data-testid="navbutton"
       {...{ onClick }}
     >
       <span className="button__header__text">{children}</span>
